5ed97bae95f710bf5be7ade6d97ae37a100324ec
Goohyun Jung <futuremaker019@gmail.com>
2019-10-23 20:36:20 +0900

    Rev.00

---
+++ 20.Lombok/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -0,0 +1,46 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Restaurant {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    @Transient
+    private List<MenuItem> menuItems;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getInformation() {
+        return name + " in " + address;
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        this.menuItems = new ArrayList<>(menuItems);
+    }
+
+    public void updateInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
