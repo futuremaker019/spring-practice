c1380e89b06d1b842aea2a7c520bd78ce057be13
GHJung <futuremaker019@gmail.com>
2019-11-20 23:10:08 +0400

    작성된 글을 게시판 목록에 추가하는 기능 구현

--- src/main/java/com/bulletinBoard/myBoard/controller/WelcomeController.java
+++ src/main/java/com/bulletinBoard/myBoard/controller/WelcomeController.java
@@ -1,16 +1,20 @@
 package com.bulletinBoard.myBoard.controller;
 
+import com.bulletinBoard.myBoard.domain.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class WelcomeController {
 
-    @GetMapping("")
-    public String hello(String name, Model model) {
+    @Autowired
+    private QuestionRepository questionRepository;
 
+    @GetMapping("")
+    public String hello(Model model) {
+        model.addAttribute("questions", questionRepository.findAll());
         return "index";
     }
 }
