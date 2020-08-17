d267c35ce063466923da088aeebdf00561e56783
futuremaker <futuremaker019@gmail.com>
2020-08-17 22:22:23 +0900

    영속/비즈니스 계층의 CRUD 구현

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -2,7 +2,7 @@ package org.zerock.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
+/*import org.apache.ibatis.annotations.Select;*/
 import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
@@ -15,4 +15,8 @@ public interface BoardMapper {
 	public void insertSelectKey(BoardVO board);
 	
 	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO boardVO);
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -33,15 +33,40 @@ public class BoardMapperTests {
 	 * log.info(board); }
 	 */
 	
+	/*
+	 * @Test public void testInsertSelectKey() { BoardVO board = new BoardVO();
+	 * board.setTitle("새로 작성하는 글 select key");
+	 * board.setContent("새로 작성하는 내용 select key"); board.setWriter("newbie");
+	 * 
+	 * boardMapper.insertSelectKey(board);
+	 * 
+	 * log.info(board); }
+	 */
+	
+	/*
+	 * @Test public void readTest() { BoardVO boardVO = boardMapper.read(5L);
+	 * 
+	 * log.info(boardVO); }
+	 */
+	
+	/*
+	 * @Test public void testDelete() { int count = boardMapper.delete(3L);
+	 * 
+	 * log.info("Delete Count : " + count); }
+	 */
+	
 	@Test
-	public void testInsertSelectKey() {
-		BoardVO board = new BoardVO();
-		board.setTitle("새로 작성하는 글 select key");
-		board.setContent("새로 작성하는 내용 select key");
-		board.setWriter("newbie");
+	public void testUpdate() {
+		BoardVO boardVO = new BoardVO();
 		
-		boardMapper.insertSelectKey(board);
+		// setter를 이용하여 입력된 값이 변수로 들어간다. mapper 객체에 전달되어 mapper.xml을 실행 
+		boardVO.setBno(5L);
+		boardVO.setTitle("수정된 제목");
+		boardVO.setContent("수정된 내용");
+		boardVO.setWriter("modified user");
 		
-		log.info(board);
+		int count = boardMapper.update(boardVO);
+		
+		log.info("Update Count : " + count);
 	}
 }
