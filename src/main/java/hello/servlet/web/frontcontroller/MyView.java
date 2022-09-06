f7507dbe25aeb08f94af86ca1a584b5dbe648cdb
futuremaker <futuremaker019@gmail.com>
2022-09-06 23:51:56 +0900

    Model 추가 - v3

---
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,33 @@
+package hello.servlet.web.frontcontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        // model에서 받아주는 값을 의미한다. (model은 key, value로 구성되어 있음을 기억하자)
+        // jsp는 setAttribute의 값을 넣어야 사용이 가능하다.
+        model.forEach((key, value) -> request.setAttribute(key, value));
+    }
+}
