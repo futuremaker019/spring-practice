545cc6b71711d6ab9be8741f65daaa5cacaa9bad
futuremaker <futuremaker019@gmail.com>
2022-09-20 23:37:26 +0900

    스프링 MVC - 실용적인 방식

---
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,52 @@
+package hello.servlet.web.springmvc.v3;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    /**
+     *
+     * Annotation 기반의 컨트롤러에서는 ModelAndView에 담아서 반환하지 않고
+     * String으로 반환해서 사용이 가능하다.
+     *
+     */
+    //@RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+    /**
+     * Model을 사용하여 데이터를 전달한다.
+     */
+    //@RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model
+    ){
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members";
+    }
+}
