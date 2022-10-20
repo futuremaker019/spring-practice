a6533b7683c2e8cf7eb4f3fb898247a795213cfc
futuremaker <futuremaker019@gmail.com>
2022-10-20 22:01:39 +0900

    상품수정

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -86,6 +86,22 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+
+        // PathVariable을 사용하면 return의 redirect에서도 itemId를 사용할 수 있다.
+        return "redirect:/basic/items/{itemId}";
+    }
+
     /**
      *  테스트용 데이터 추가
      */
