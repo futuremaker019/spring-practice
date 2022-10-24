b2ab74c85717b528881e5d71d970241e474718a6
futuremaker <futuremaker019@gmail.com>
2022-10-24 22:58:07 +0900

    로그인로그인 ì처리, 쿠키/로그인처리하기-쿠키사용

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,61 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    //@GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    /**
+     * Spring에서 제공하는 @CookieValue 를 이용하여 쿠키를 받아온다.
+     *      required=false를 하여 로그인 하지 않은 사용자도 들어올수 있게 만들어준다.
+     */
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+
+        // 쿠키값이 없으면 home으로 보낸다.
+        if (memberId == null) {
+            return "home";
+        }
+
+        // 로그인 상태이면 loginHome 화면으로 이동시킨다.
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
