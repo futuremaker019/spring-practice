e08df96c0fc8b3074c01faa196946b1eecf842b1
futuremaker019 <futuremaker019@gmail.com>
2022-10-27 23:56:34 +0900

    chaged added

--- login/src/main/java/hello/login/domain/login/LoginService.java
+++ login/src/main/java/hello/login/domain/login/LoginService.java
@@ -22,6 +22,7 @@ public class LoginService {
 //            return null;
 //        }
 
+
         return memberRepository.findByLoginId(loginId)
                 .filter(member -> member.getPassword().equals(password))
                 .orElse(null);
