324470ef4455d0e382fb3fed479474701b27704d
futuremaker019 <futuremaker019@gmail.com>
2023-01-02 20:38:48 +0900

    공통 인터페이스 기능/순수 JPA 기반 리포지토리 만들기

--- JPA/spring-data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ JPA/spring-data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -6,6 +6,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,7 +21,27 @@ public class MemberJpaRepository {
         return member.getId();
     }
 
-    public Member findById(Long id) {
+    public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member); // null일수도 아닐수도 있다는 것을 명시한다.
+    }
+
+    public List<Member> findAll() {
+        // JPQL
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
     }
 }
