74b59381008bafebd039064f470d9b3fb75aa8bb
futuremaker019 <futuremaker019@gmail.com>
2023-01-03 20:11:52 +0900

    쿼리 메소드 기능/@Query, 리포지토리 메소ㅗ드에 쿼리 정의하기

--- JPA/spring-data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ JPA/spring-data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -8,9 +8,18 @@ import study.datajpa.entity.Member;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findTop3HelloBy();
+
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     // NamedQuery는 실무에서 거의 사용하지 않는다.
 //    @Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
+
+    /**
+     * 쿼리내부에 오타가 있으면 애플리케이션 로딩 시점에 에러를 발생시킨다.
+     *  애플리케이션 로딩시 정적쿼리들을 다 파싱한다. 파싱시 문법적 오류를 찾으면 에러를 발생시킨다.
+     */
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
