d5accb8ceb34da597b086763db529b15509ceeda
futuremaker019 <futuremaker019@gmail.com>
2023-01-03 19:55:14 +0900

    JPA NamedQuery

--- JPA/spring-data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ JPA/spring-data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -8,6 +8,10 @@ import javax.persistence.*;
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})  // 연관관계필드는 toString에 포함시키지말자
+@NamedQuery(
+        name="Member.findByUsername",
+        query="select m from Member m where m.username= :username"
+)
 public class Member {
 
     @Id @GeneratedValue
--- JPA/spring-data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ JPA/spring-data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,12 +1,16 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    List<Member> findByUsername(String username);
-
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    // NamedQuery는 실무에서 거의 사용하지 않는다.
+//    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
 }
