659277bb8f1bd8d6fb28879eb735d8f760bd6d82
futuremaker019 <futuremaker019@gmail.com>
2023-01-03 20:22:34 +0900

    쿼리 메소드 기능/@Query, 값, DTO 조회하기

--- JPA/spring-data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ JPA/spring-data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -22,4 +23,15 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
      */
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    /**
+     * dto를 명시하여 dto를 반환하게 만들어준다.
+     *  dto의 패키지 경로를 모두 작성해줘야 한다.
+     * @return
+     */
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
