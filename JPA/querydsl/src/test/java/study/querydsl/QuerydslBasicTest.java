cb604f39d7087742ba4884181135b15673b24e9c
futuremaker019 <futuremaker019@gmail.com>
2023-02-01 19:11:24 +0900

    기본 문법/조인-기본 조인

--- JPA/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ JPA/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -219,4 +219,46 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
+
+    /**
+     * 팀 A에 소속된 모든 회원을 찾는다
+     */
+    @Test
+    public void join() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+//                .join(member.team, team)
+                .leftJoin(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /**
+     *
+     * 세타 조인
+     * 회원의 이름이 팀 이름과 동일한 같은 회원 조회
+     *    모든회원과 모든팀을 가져와 조인시킨후 where 절에서 필터링하는 방식이다.
+     * outer 조인이 불가능함 - 연관관계가 없기때문에 불가능 (그러나 on을 사용하면 가능)
+     *
+     */
+    @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
 }
