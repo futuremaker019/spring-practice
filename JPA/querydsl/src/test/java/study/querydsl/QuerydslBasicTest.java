f01c002a9007213537dd1c3f7ff739ddbde20873
futuremaker019 <futuremaker019@gmail.com>
2023-02-05 23:04:53 +0900

    기본 문법/Case 문

--- JPA/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ JPA/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.aspectj.lang.annotation.Before;
@@ -529,5 +530,66 @@ public class QuerydslBasicTest {
          *             member member0_
          *
          */
+    }
+
+    @Test
+    public void basicCase() {
+        // given
+        List<String> result = queryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+
+        /**
+         * sql
+         *
+         *         select
+         *             case
+         *                 when member0_.age=? then ?
+         *                 when member0_.age=? then ?
+         *                 else '기타'
+         *             end as col_0_0_
+         *         from
+         *             member member0_
+         */
+
+    }
+
+    @Test
+    public void complexCase() {
+        // given
+        List<String> result = queryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타")
+                ).from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+
+        /**
+         * sql
+         *
+         *        select
+         *             case
+         *                 when member0_.age between ? and ? then ?
+         *                 when member0_.age between ? and ? then ?
+         *                 else '기타'
+         *             end as col_0_0_
+         *         from
+         *             member member0_
+         *
+         */
+
     }
 }
