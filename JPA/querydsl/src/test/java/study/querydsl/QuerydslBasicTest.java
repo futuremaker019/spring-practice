fb68f54a2515fba40d42f1a53a071e14cdd0ff59
futuremaker <futuremaker019@gmail.com>
2023-02-09 00:01:14 +0900

    중급 문법/동적쿼리-BooleanBuilder사용

--- JPA/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ JPA/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -875,7 +876,41 @@ public class QuerydslBasicTest {
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
         }
+    }
 
+    @Test
+    public void dynamicQuery_BooleanBuilder() throws Exception {
+        String usernameParam = "member1";
+        Integer ageParam = 10;
 
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+
+        /**
+         *     select
+         *         member1
+         *     from
+         *         Member member1
+         *     where
+         *         member1.username = ?1
+         *         and member1.age = ?2
+         */
+    }
+
+    // BooleanBuilder를 사용하여 동적쿼리를 생성한다.
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
     }
 }
