28e977362f7796c4170614e4f440692183f79ffd
Goohyun Jung <futuremaker019@gmail.com>
2023-07-29 21:42:00 +0900

    `@ControllerAdvice`를 이용한 에러 핸들링

--- spring-basic/basic2/exception/src/main/java/com/practice/exception/api/ApiV2Controller.java
+++ spring-basic/basic2/exception/src/main/java/com/practice/exception/api/ApiV2Controller.java
@@ -1,13 +1,12 @@
 package com.practice.exception.api;
 
-import com.practice.exception.exHandler.ErrorResult;
 import com.practice.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -26,27 +25,6 @@ public class ApiV2Controller {
         }
 
         return new MemberDto(id, "hello " + id);
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandler(IllegalArgumentException e) {
-        log.error("{exceptionHandler} ex", e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
-        log.error("{exceptionHandler} ex", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult exHandler(Exception e) {
-        log.error("{exceptionHandler} ex", e);
-        return new ErrorResult("EX", "내부 오류");
     }
 
     @Data
