cde119fc4a115aa2f0724f5a4314eb8a2efbf8bc
Goohyun Jung <futuremaker019@gmail.com>
2023-11-25 19:29:03 +0900

    스프링 핵심원리 - 고급편 init

---
+++ spring-advanced_02/src/main/java/core/advanced/SpringAdvanced02Application.java
@@ -0,0 +1,13 @@
+package core.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvanced02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvanced02Application.class, args);
+    }
+
+}
