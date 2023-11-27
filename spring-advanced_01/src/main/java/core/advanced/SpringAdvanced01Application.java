a908f1a888a4e530480bf89ca6f35942ddbd27b9
futuremaker019 <futuremaker019@gmail.com>
2023-11-27 22:56:17 +0900

    스프링 핵심원리/프로젝트 init

---
+++ spring-advanced_01/src/main/java/core/advanced/SpringAdvanced01Application.java
@@ -0,0 +1,13 @@
+package core.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvanced01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvanced01Application.class, args);
+    }
+
+}
